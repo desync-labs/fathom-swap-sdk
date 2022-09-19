8f8f1aabd006c79b5d3c93afbf554f5e9d750825
ztshort <ztshort@hotmail.com>
2022-09-19 15:51:48 -0500

    xdc integration

--- src/constants.ts
+++ src/constants.ts
@@ -1,14 +1,55 @@
 import JSBI from 'jsbi'
 
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+// exports for external consumption
+export type BigintIsh = JSBI | bigint | string
 
-export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
+export enum ChainId {
+  MAINNET = 1,
+  GOERLI = 5,
+
+  XDC = 50,
+  AXDC = 51
+}
+
+export enum TradeType {
+  EXACT_INPUT,
+  EXACT_OUTPUT
+}
+
+export enum Rounding {
+  ROUND_DOWN,
+  ROUND_HALF_UP,
+  ROUND_UP
+}
+
+export const FACTORY_ADDRESS: { [key: string]: string } = {
+  [ChainId.MAINNET]: '0x0000000000000000000000000000000000000000',
+  [ChainId.GOERLI]: '0x9582b403791662a0727D162e328Fe84CaCd9978D',
+  [ChainId.XDC]: '0x0000000000000000000000000000000000000000',
+  [ChainId.AXDC]: '0xB0f3c7BFC88fACAD5825eA8AB758b1FA8038b71C'
+}
+
+export const INIT_CODE_HASH = '0x3e8224d35e34f2208a5e0af408b2af63cb5c0c95d73f68b75c2de5e64d1246aa'
 
 export const MINIMUM_LIQUIDITY = JSBI.BigInt(1000)
 
 // exports for internal consumption
 export const ZERO = JSBI.BigInt(0)
 export const ONE = JSBI.BigInt(1)
+export const TWO = JSBI.BigInt(2)
+export const THREE = JSBI.BigInt(3)
 export const FIVE = JSBI.BigInt(5)
+export const TEN = JSBI.BigInt(10)
+export const _100 = JSBI.BigInt(100)
 export const _997 = JSBI.BigInt(997)
 export const _1000 = JSBI.BigInt(1000)
+
+export enum SolidityType {
+  uint8 = 'uint8',
+  uint256 = 'uint256'
+}
+
+export const SOLIDITY_TYPE_MAXIMA = {
+  [SolidityType.uint8]: JSBI.BigInt('0xff'),
+  [SolidityType.uint256]: JSBI.BigInt('0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff')
+}
--- test/constants.test.ts
+++ test/constants.test.ts
@@ -1,11 +1,11 @@
-import { INIT_CODE_HASH } from './constants'
+import { INIT_CODE_HASH } from '../src/constants'
 
-import { bytecode } from '@fathomswap/v2-core/build/FathomSwapV2Pair.json'
-import { keccak256 } from '@ethersproject/solidity'
+import { bytecode } from 'fathomswap-contracts/artifacts/contracts/core/FathomswapPair.sol/FathomswapPair.json'
+import { keccak256 } from '@baldyash/solidity'
 
 // this _could_ go in constants, except that it would cost every consumer of the sdk the CPU to compute the hash
 // and load the JSON.
-const COMPUTED_INIT_CODE_HASH = keccak256(['bytes'], [`0x${bytecode}`])
+const COMPUTED_INIT_CODE_HASH = keccak256(['bytes'], [`${bytecode}`])
 
 describe('constants', () => {
   describe('INIT_CODE_HASH', () => {
