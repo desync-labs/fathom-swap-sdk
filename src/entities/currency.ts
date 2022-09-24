0856258f889c299e37987cf1ff282245f3a378d4
ztshort <ztshort@hotmail.com>
2022-09-24 00:49:16 -0500

    Added XDC currency and restored ETHER currency

--- src/entities/currency.ts
+++ src/entities/currency.ts
@@ -16,7 +16,9 @@ export class Currency {
   /**
    * The only instance of the base class `Currency`.
    */
-  public static readonly ETHER: Currency = new Currency(18, 'XDC', 'XDC')
+  public static readonly ETHER: Currency = new Currency(18, 'ETHER', 'ETHER')
+
+  public static readonly XDC: Currency = new Currency(18, 'XDC', 'XDC')
 
   /**
    * Constructs an instance of the base class `Currency`. The only instance of the base class `Currency` is `Currency.ETHER`.
